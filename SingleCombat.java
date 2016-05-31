7ed65a56d60de52ac62f1b17f0c854b2c243ccbd
Austin Kreulach <akreulach@gmail.com>
2016-05-31 01:47:11 -0500

    SingleCombat fixed.

--- SingleCombat.java
+++ SingleCombat.java
@@ -6,7 +6,7 @@ public class SingleCombat {
 	static int count1,count2;
 	public static void main(String[] args){
 		count1=count2=0;
-		for(int n=0;n<10000;n++)
+		for(int n=0;n<1000;n++)
 			fight();
 		System.out.println("Titanus won "+count2+" times, while Tim won "+count1+" times.");
 	}
@@ -15,7 +15,7 @@ public class SingleCombat {
 		int[] smol = {10,10,10,8,8,8};
 		Soldier a = new Soldier("Titanus",beeg);
 		//System.out.println(a.getName());
-		Soldier b = new Soldier("Tim",beeg);
+		Soldier b = new Soldier("Tim",smol);
 		//System.out.println(b.getName());
 		//System.out.println("************************");
 		//System.out.println("Initiative\n");
@@ -30,26 +30,34 @@ public class SingleCombat {
 			//System.out.println(a.getName()+" takes the initiative and strikes first.");
 			fist=true;
 		}
-		else
 			//System.out.println(b.getName()+" takes the initiative and strikes first.");
 		//System.out.println();
 		while(a.HP>0&&b.HP>0){
 			if(fist){
-				if(a.BAB+a.statx[0]+d.d(20)>=b.AC)
-					b.HP-=d.d(6)+a.statx[0];
+				if(a.BAB+a.statx[0]+DieRoller.d(20)>=b.AC)
+					b.HP-=DieRoller.d(6)+a.statx[0];
+				if(b.BAB+b.statx[0]+DieRoller.d(20)>=a.AC)
+					a.HP-=DieRoller.d(6)+b.statx[0];
 			}
-			if(b.BAB+b.statx[0]+d.d(20)>=a.AC)
-				a.HP-=d.d(6)+b.statx[0];
-			if(a.BAB+a.statx[0]+d.d(20)>=b.AC)
-				b.HP-=d.d(6)+a.statx[0];
+			else{
+				if(b.BAB+b.statx[0]+DieRoller.d(20)>=a.AC)
+					a.HP-=DieRoller.d(6)+b.statx[0];
+				if(a.BAB+a.statx[0]+DieRoller.d(20)>=b.AC)
+					b.HP-=DieRoller.d(6)+a.statx[0];
+			}			
+			//System.out.println("\nRound over, "+a.getName()+" has "+a.HP+" hitpoints left.");
+			//System.out.println(b.getName()+" has "+b.HP+" hitpoints left.");
 		}
-		if(a.HP<=0){
+		if(a.HP<=0&&b.HP>0){
 			//System.out.println(b.getName()+" has slain "+a.getName()+" and won!");
 			count1++;
 		}
-		else{
+		else if(b.HP<=0&&a.HP>0){
 			//System.out.println(a.getName()+" has slain "+b.getName()+" and won!");
 			count2++;
+		}
+		else{
+			fight();
 		}
 	}
 }
