925123bcb14eb55eee73213e5dc17acb9a01effa
Austin Kreulach <akreulach@gmail.com>
2016-05-30 22:00:53 -0500

    Add files via upload

---
+++ Resolver.java
@@ -0,0 +1,19 @@
+package baseCase;
+/*
+ * Morale
+ * A%=allies_alive/(allies_alive+allies_dead)
+ * E%=enemies_alive/(enemies_alive+enemies_dead)
+ * L%=A%/E%
+ * 1st levels run at L%=.50
+ * 2nd =.42
+ * 3rd =.34
+ * 4th =.26
+ * 5th =.18
+ * 6th =.10
+ */
+
+//Class should take two Army objects, calculate rounds until one has either been destroyed or 
+//had their morale shattered, then return new Army objects containing the survivors.
+public class Resolver {
+	
+}
